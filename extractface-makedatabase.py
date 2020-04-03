9ad4a1c8ca9bc89128246d3099d62877f1f718ce
Rohan Kapoor <32823172+rhnkpr@users.noreply.github.com>
2020-04-03 15:48:39 -0500

    Add files via upload

---
+++ extractface-makedatabase.py
@@ -0,0 +1,88 @@
+
+from os import listdir
+from os.path import isdir
+from PIL import Image
+from numpy import asarray
+from numpy import savez_compressed
+from matplotlib import pyplot
+from mtcnn.mtcnn import MTCNN
+
+# extract a single face from a given photograph
+def extract_face(filename, required_size=(160, 160)):
+	# load image from file
+	image = Image.open(filename)
+	# convert to RGB, if needed
+	image = image.convert('RGB')
+	# convert to array
+	pixels = asarray(image)
+	# create the detector, using default weights
+	detector = MTCNN()
+	# detect faces in the image
+	results = detector.detect_faces(pixels)
+	print(filename)
+	print(results)
+
+	if len(results) == 0:
+		return
+	print(results[0]['box'])
+	print("---------------------")
+	# extract the bounding box from the first face
+	x1, y1, width, height = results[0]['box']
+	# bug fix
+	x1, y1 = abs(x1), abs(y1)
+	x2, y2 = x1 + width, y1 + height
+	# extract the face
+	face = pixels[y1:y2, x1:x2]
+	# resize pixels to the model size
+	image = Image.fromarray(face)
+	image = image.resize(required_size)
+	face_array = asarray(image)
+	return face_array
+
+# load images and extract faces for all images in a directory
+def load_faces(directory):
+	faces = list()
+	# enumerate files
+	for filename in listdir(directory):
+		# path
+		path = directory + filename
+		# get face
+		face = extract_face(path)
+		# store
+		if face is not None:
+			faces.append(face)
+	""" plt.figure()
+		plt.imshow(faces)
+		plt.colorbar()
+		plt.grid(False)	
+		plt.show() """
+	return faces
+
+# load a dataset that contains one subdir for each class that in turn contains images
+def load_dataset(directory):
+	X, y = list(), list()
+	# enumerate folders, on per class
+	for subdir in listdir(directory):
+		# path
+		path = directory + subdir + '/'
+		# skip any files that might be in the dir
+		if not isdir(path):
+			continue
+		# load all faces in the subdirectory
+		faces = load_faces(path)
+		# create labels
+		labels = [subdir for _ in range(len(faces))]
+		# summarize progress
+		print('>loaded %d examples for class: %s' % (len(faces), subdir))
+		# store
+		X.extend(faces)
+		y.extend(labels)
+	return asarray(X), asarray(y)
+
+# load train dataset
+trainX, trainy = load_dataset('/home/rohan/SeniorDesign/FaceDataset/train/')
+print(trainX.shape, trainy.shape)
+# load test dataset
+testX, testy = load_dataset('/home/rohan/SeniorDesign/FaceDataset/val/')
+# save arrays to one file in compressed format
+savez_compressed('face-dataset.npz', trainX, trainy, testX, testy)
---
+++ face-embeddings.py
@@ -0,0 +1,54 @@
+# calculate a face embedding for each face in the dataset using facenet
+from numpy import load
+from numpy import expand_dims
+from numpy import asarray
+from numpy import savez_compressed
+import numpy as np
+from keras.models import load_model
+
+
+## Load function is broken - FIX
+# save np.load
+np_load_old = np.load
+# modify the default parameters of np.load
+np.load = lambda *a,**k: np_load_old(*a, allow_pickle=True, **k)
+
+# get the face embedding for one face
+def get_embedding(model, face_pixels):
+	# scale pixel values
+	face_pixels = face_pixels.astype('float32')
+	# standardize pixel values across channels (global)
+	mean, std = face_pixels.mean(), face_pixels.std()
+	face_pixels = (face_pixels - mean) / std
+	# transform face into one sample
+	samples = expand_dims(face_pixels, axis=0)
+	# make prediction to get embedding
+	yhat = model.predict(samples)
+	return yhat[0]
+
+# load the face dataset
+data = np.load('face-dataset.npz')
+trainX, trainy, testX, testy = data['arr_0'], data['arr_1'], data['arr_2'], data['arr_3']
+print('Loaded: ', trainX.shape, trainy.shape, testX.shape, testy.shape)
+# load the facenet model
+model = load_model('facenet_keras.h5')
+print('Loaded Model')
+# convert each face in the train set to an embedding
+newTrainX = list()
+for face_pixels in trainX:
+	embedding = get_embedding(model, face_pixels)
+	newTrainX.append(embedding)
+newTrainX = asarray(newTrainX)
+print(newTrainX.shape)
+# convert each face in the test set to an embedding
+newTestX = list()
+for face_pixels in testX:
+	embedding = get_embedding(model, face_pixels)
+	newTestX.append(embedding)
+newTestX = asarray(newTestX)
+print(newTestX.shape)
+# save arrays to one file in compressed format
+savez_compressed('face-embeddings.npz', newTrainX, trainy, newTestX, testy)
+
+# restore np.load for future normal usage
+np.load = np_load_old
