522ef490971dd1c44e09edff72e8e04518877c21
Rohan Kapoor <32823172+rhnkpr@users.noreply.github.com>
2020-04-04 21:56:40 -0500

    Add files via upload

--- extractface-makedatabase.py
+++ extractface-makedatabase.py
@@ -6,15 +6,78 @@ from numpy import asarray
 from numpy import savez_compressed
 from matplotlib import pyplot
 from mtcnn.mtcnn import MTCNN
+from glob import glob
+import os.path
+#import rawpy, imageio
+import cv2
+import math
+
+#height and width of output
+width_, height_  = 480, 480
+# this allows me to include more area around the face (multipication factor)
+exp_x,exp_y = 1.3,1.3
+
+#load face finding classifiers
+#///////////////////////////////////////////////////////////////////////////////
+Classifier = [
+'haarcascade_frontalface_default.xml',  #0
+'haarcascade_frontalface_alt.xml',      #1
+'haarcascade_frontalface_alt2.xml',     #2
+'haarcascade_frontalface_alt_tree.xml', #3
+'haarcascade_eye.xml',                  #4
+'haarcascade_eye_tree_eyeglasses.xml',  #5
+'haarcascade_lefteye_2splits.xml',      #6
+'haarcascade_righteye_2splits.xml',     #7
+'haarcascade_profileface.xml']          #8
+#///////////////////////////////////////////////////////////////////////////////
+
+# Load the cascade
+face_cascade = cv2.CascadeClassifier('./XML_classifiers/'+Classifier[2])
+profile_cascade = cv2.CascadeClassifier('./XML_classifiers/'+Classifier[8])
 
 # extract a single face from a given photograph
 def extract_face(filename, required_size=(160, 160)):
+	""" 
 	# load image from file
 	image = Image.open(filename)
 	# convert to RGB, if needed
 	image = image.convert('RGB')
 	# convert to array
 	pixels = asarray(image)
+
+ """	
+	# Read the input image
+	img_orig = cv2.imread(filename)
+
+	flipped = False
+	faces,img,width,height = findOneface(img_orig)
+
+	# this crops the face out and scales it as a box
+	for (x,y,w,h) in faces:
+		#locate center of box
+		x_cent, y_cent = (x+(w//2)), (y+(h//2))
+		#establish x,y,w,h for scale factors
+		w2, h2 = int(math.floor(w*exp_x)), int(math.floor(h*exp_y))
+		x2, y2 = (x_cent-(w2//2)), (y_cent-(h2//2))
+		#check if it's to close to the edge of the frame
+		if (x2 < 0 or x2+w2 > width) or (y2 < 0 and y2+h2 > height):
+			x2, y2 = x, y
+			w2, h2 = w, h
+		#produce cropped image of face
+		crop_img = img[y2:y2+h2, x2:x2+w2]
+		crop_resized = cv2.resize(crop_img, (width_,height_), interpolation = cv2.INTER_AREA)
+		#cv2.imshow("cropped", crop_resized)
+		print('successfully found face')
+		if flipped == False:
+			face_array = asarray(crop_resized)
+			return face_array
+		else:
+			print("image was flipped back")
+			face_array = asarray(cv2.flip(crop_resized))
+			return face_array
+	return None
+	
+""" 
 	# create the detector, using default weights
 	detector = MTCNN()
 	# detect faces in the image
@@ -38,6 +101,50 @@ def extract_face(filename, required_size=(160, 160)):
 	image = image.resize(required_size)
 	face_array = asarray(image)
 	return face_array
+ """
+def reduceSize(img, scale_factor=0.5):
+    width = img.shape[1]
+    height = img.shape[0]
+    if (img.shape[0] >= 1500) or (img.shape[1] >= 1500):
+        width = int(math.floor(img.shape[1]*scale_factor))
+        height = int(math.floor(img.shape[0]*scale_factor))
+        img = cv2.resize(img, (width,height), interpolation = cv2.INTER_AREA)
+    elif scale_factor != 0.5:
+        width = int(math.floor(img.shape[1]*scale_factor))
+        height = int(math.floor(img.shape[0]*scale_factor))
+        img = cv2.resize(img, (width,height), interpolation = cv2.INTER_AREA)
+    return img,width,height
+
+def findOneface(img_orig):
+    global flipped
+    img,width,height = reduceSize(img_orig)
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(gray, 1.1, 6)
+    if len(faces) > 1:
+        #f.write('Possible Error: Double check file       '+i+'\n')
+        img,width,height = reduceSize(img_orig,0.3)
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        faces = face_cascade.detectMultiScale(gray, 1.1, 6)
+        if len(faces) < 1:
+            faces = profile_cascade.detectMultiScale(gray, 1.1, 6)
+            if len(faces) < 1:
+                img,width,height = reduceSize(img_orig)
+                gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+                faces = face_cascade.detectMultiScale(gray, 1.1, 6)
+    if len(faces) < 1:
+        faces = profile_cascade.detectMultiScale(gray, 1.1, 6)
+    if len(faces) < 1:
+        img = cv2.flip(img, 1)
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        faces = profile_cascade.detectMultiScale(gray, 1.1, 6)
+        flipped = True
+        print("image was flipped once")
+        if len(faces) < 1:
+            faces = face_cascade.detectMultiScale(gray, 1.1, 6)
+    if len(faces) < 1:
+        print('Error: no face found in file')
+        #f.write('Error: no face found in file       '+i+'\n')
+    return faces,img,width,height
 
 # load images and extract faces for all images in a directory
 def load_faces(directory):
